1babf081777ea8e15347c43880de976e4880935d
chenson666 <50819196+chenson666@users.noreply.github.com>
2020-05-01 23:12:16 +0800

    mg

---
+++ adservice.google.com/adsid/integrator.js
@@ -0,0 +1,7 @@
+processGoogleToken({
+    "newToken": "ChAI8JGv9QUQ4MDN6rKi-I8yEkcA_sPkP_tNSiv31iY7aPGFdCtg16VFErymvw_a_fEp2uIkv6mQtcUlyOZO5k3k7514CotTo0kboa8Lg-al0XxLdoYueXDr9Q",
+    "validLifetimeSecs": 300,
+    "freshLifetimeSecs": 300,
+    "1p_jar": "2020-5-1-13",
+    "pucrd": ""
+});
